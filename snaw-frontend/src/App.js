deaf17eb70959cb668d18d57999b056f56cdb3ca
Mike Ewers <creativedevvers@gmail.com>
2019-12-05 13:52:45 -0700

    added array from back-end (Testing back-end input)

--- snaw-frontend/src/App.js
+++ snaw-frontend/src/App.js
@@ -5,12 +5,16 @@ import AnalyzeButton from './components/AnalyzeButton';
 import UploadButton from './components/UploadButton';
 
 function App() {
+
+  var randomNum = Math.floor(Math.random() * 5);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        //First we test a string from the flask back-end.
         <p>Token received from Backend = {window.token}</p>
-        <p>Test information array [1, 2, 3, 4, 5]  at position 1 = {window.array[1]}</p>
+        //We then will test receiving an array from the back-end and display its positions.
+        <p>Test information array [1, 2, 3, 4, 5]  at position {randomNum} = {window.array[randomNum]}</p>
         <p>
           <b>Soundscape Noise Analysis Workbench</b>
         </p>
