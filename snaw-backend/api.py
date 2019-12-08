959b917989b7682c00fe06bfd15f29261a4be9d4
Mike Ewers <creativedevvers@gmail.com>
2019-12-08 00:48:11 -0700

    added spectro and CityNet analysis to homepage (example)

--- snaw-backend/api.py
+++ snaw-backend/api.py
@@ -1,22 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import os
 import sys
 import subprocess
+from CityNet.demo import runFunction
+from get_spectrogram import runScript
 app = Flask("__main__")
 app.config["DEBUG"] = True
 
 
 
+
 @app.route('/')
 def home():
+    stringMessage =  "Hello from Flask :)"
     arrayOfNums = [1, 2, 3, 4, 5]
 
-    """ 
-    Upon rendering the index.html, we also send in a string message
-    and the above arrayOfNums
-    """
-    return render_template('index.html', message = "Hello from Flask :)", array = arrayOfNums)
+    spectroImg = runScript()
+    analysisImg = runFunction()
 
+    return render_template("index.html", message = stringMessage, array = arrayOfNums, spectroImg = spectroImg, analysisImg =  analysisImg)
 
 #this is an example of how the uploading process may happen. More research required.
 @app.route("/uploadFile", methods=['POST'])
@@ -25,9 +27,15 @@ def upload():
 
 @app.route("/results")
 def analyze():
-    analysisOutput = subprocess.check_output([sys.executable, "CityNet/demo.py", "forest_path126.wav"])
-    print(analysisOutput)
-    return
+    #analysisOutput = subprocess.check_output([sys.executable, "CityNet/demo.py", "CityNet/demo/forest_path126.wav"])
+    #Run CityNet demo.py script
+    runFunction()
+    #show the pdf provided by the script
+    try:
+        return send_file('CityNet/demo/predictions.pdf',
+                         attachment_filename='predictions.pdf')
+    except Exception as e:
+        return str(e)
 
 print('Starting Flask!')
 app.run()
