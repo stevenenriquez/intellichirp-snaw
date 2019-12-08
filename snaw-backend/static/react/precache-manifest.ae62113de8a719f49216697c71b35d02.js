ac207f28f28ece3c7dc786680e77912945e70b9e
Mike Ewers <creativedevvers@gmail.com>
2019-12-08 01:13:39 -0700

    Static updates

---
+++ snaw-backend/static/react/precache-manifest.ae62113de8a719f49216697c71b35d02.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ccefb8dba8a87af28f38fb7dbb885c",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "99b6df27463508f134d5",
+    "url": "/static/react/css/main.0f949a12.chunk.css"
+  },
+  {
+    "revision": "8d36b3fc095a5d129b90",
+    "url": "/static/react/js/2.36186d4b.chunk.js"
+  },
+  {
+    "revision": "99b6df27463508f134d5",
+    "url": "/static/react/js/main.20670229.chunk.js"
+  },
+  {
+    "revision": "a37a52e938f4ebc27da0",
+    "url": "/static/react/js/runtime-main.14a87568.js"
+  },
+  {
+    "revision": "1f13aa3f87c85b7e7811fd0b2abebaf4",
+    "url": "/static/react/media/AnthroClassifyRainforest.1f13aa3f.png"
+  },
+  {
+    "revision": "8090d8872d44449997a21a7383c4fd3d",
+    "url": "/static/react/media/ExpandedClassifyRainforest.8090d887.png"
+  },
+  {
+    "revision": "14444155c0f57c18cc69a42b3d7d1de7",
+    "url": "/static/react/media/SmallClassifyRainforest.14444155.png"
+  },
+  {
+    "revision": "7e1954b415b996a6e57c4861257e0741",
+    "url": "/static/react/media/logo_small.7e1954b4.png"
+  },
+  {
+    "revision": "17942d01e1825bae3b2f4b2f880c97d4",
+    "url": "/static/react/media/rainforest_specto.17942d01.png"
+  }
+]);
