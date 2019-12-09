33f77b493b2b7472405e30b2485aeafa471e6981
Steven Enriquez <stevenenriquez123@gmail.com>
2019-12-09 14:21:31 -0700

    frontend for uploading files to server

--- snaw-frontend/src/App.js
+++ snaw-frontend/src/App.js
@@ -1,35 +1,62 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './img/logo_small.png';
 import './App.css';
 import AnalyzeButton from './components/AnalyzeButton';
-import UploadButton from './components/UploadButton';
 
-function App() {
+class App extends React.Component {
 
-  var randomNum = Math.floor(Math.random() * 5);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        //First we test a string from the flask back-end.
-        <p>Token received from Backend = {window.token}</p>
-        //We then will test receiving an array from the back-end and display its positions.
-        <p>Test information array [1, 2, 3, 4, 5]  at position {randomNum} = {window.array[randomNum]}</p>
-        <p>
-          <b>Soundscape Noise Analysis Workbench</b>
-        </p>
-        <br/>
-        <div> 
-          Drag and drop WAV files here, or click the Upload button to select file/s.
-          <UploadButton/>
-          <br/><br/>
-        </div>
-        <div> 
-          <AnalyzeButton/>
-        </div>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {selectedFile: []}
+  }
+
+  fileSelectedHandler = event => {
+    event.preventDefault()
+    let file = event.target.files[0];
+    this.state.selectedFile.push(file)
+  }
+
+  fileUploadHandler = () => {
+    const fData = new FormData();
+    fData.append('file', this.state.selectedFile[0]);
+    fData.append('filename', this.state.selectedFile[0].name);
+    console.log(fData);
+    axios({
+      method: 'post',
+      url: 'http://127.0.0.1:5000/upload',
+      data: fData,
+    })
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <b>Soundscape Noise Analysis Workbench</b>
+          </p>
+          <br/>
+          <form action="http://127.0.0.1:5000/upload" method="post">
+            <div> 
+              Drag and drop WAV files here, or click the Upload button to select file/s. <br/>
+              {/* Choose file/s */}
+              <input type="file" accept="audio/wav" onChange={this.fileSelectedHandler} />
+              {/* Upload file/s */}
+              <button onClick={this.fileUploadHandler}>Upload</button>
+              <br/><br/>
+            </div>
+          </form>
+          <div> 
+            <AnalyzeButton/>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
