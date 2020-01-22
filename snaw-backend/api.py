ebc629e41325313ab48ac1be82b86ac280b5b1b2
Mike-PC\Mike <mje85@nau.edu>
2020-01-22 16:38:33 -0700

    API Cleanup

--- snaw-backend/api.py
+++ snaw-backend/api.py
@@ -19,9 +19,6 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route('/')
 def home():
-
-    spectroImg = get_spectrogram()
-
     return render_template("index.html")
 
 def allowed_file(filename):
@@ -35,31 +32,6 @@ def upload_file():
       filename = secure_filename(f.filename)
       f.save(os.path.join(UPLOAD_FOLDER, filename))
       return redirect('http://127.0.0.1:5000')
-
-#this is an example of how the uploading process may happen. More research required.
-@app.route("/upload", methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            print('[!!!!!!] No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            print('[!!!!!!] No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            print( '[!!!!!!!!!!!]' + app.config['UPLOAD_FOLDER'] )
-            print( '[!!!!!!!!!!!!!]' + app.instance_path )
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file.save(os.path.join(app.instance_path, UPLOAD_FOLDER, filename))
-            return redirect('http://127.0.0.1:5000')
-    return redirect('http://127.0.0.1:5000')
 
 @app.route("/results")
 def analyze():
