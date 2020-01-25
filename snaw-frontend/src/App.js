fc9b10f200aa74fc6faa9a2cc7a0f70b86ab50ce
Mike Ewers <creativedevvers@gmail.com>
2020-01-24 18:50:48 -0700

    App.js now takes in multiple files on the html form. The event handler then pushes the filelist object to the selectedfile state

--- snaw-frontend/src/App.js
+++ snaw-frontend/src/App.js
@@ -16,9 +16,9 @@ class App extends React.Component {
   }
 
   fileSelectedHandler = event => {
-    event.preventDefault();
-    let file = event.target.files[-1];
-    this.state.selectedFile.push(file);
+    //event.preventDefault();
+    this.state.selectedFile = Array.from(event.target.files)
+    console.log(this.state.selectedFile);
   };
 
   render() {
@@ -37,7 +37,7 @@ class App extends React.Component {
             {/*<InputLabel htmlFor="my-input">Upload File</InputLabel>*/}
             <label htmlFor='my-input'><Button variant="outlined" color='#3f5a14' component='span'>Upload Audio
               File</Button></label>
-            <Input id="my-input" aria-describedby="my-helper-text" type='file' name='file' style={{display: 'none'}}/>
+            <input id="my-input" aria-describedby="my-helper-text" type='file' multiple={true} onChange={this.fileSelectedHandler} name='file' style={{display: 'none'}}/>
             <FormHelperText id="my-helper-text">
                 Drag and drop WAV files here, or click the Upload button to select file/s.
             </FormHelperText>
