54198fcec4b1ecf65beee992c8994cc279e97d22
Mike Ewers <creativedevvers@gmail.com>
2020-02-14 05:04:57 -0700

    AnalyzeButton.js converted to class to make the button istelf "on" or "off" depending on if there are available files.

--- snaw-frontend/src/components/AnalyzeButton.js
+++ snaw-frontend/src/components/AnalyzeButton.js
@@ -2,34 +2,43 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import $ from "jquery";
+import { withStyles} from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    color: 'white',
-    fontSize: '1em',
-    backgroundColor: '#3f5a14',
-    margin: theme.spacing(1),
-    paddingLeft: theme.spacing(4),
-    paddingRight: theme.spacing(4),
-    '&:hover': {
-      background: '#2e420e',
-   },
-  },
-}));
+const useStyles = theme => ({
+    button: {
+        color: 'white',
+        fontSize: '1em',
+        backgroundColor: '#3f5a14',
+        margin: theme.spacing(1),
+        paddingLeft: theme.spacing(4),
+        paddingRight: theme.spacing(4),
+        '&:hover': {
+            background: '#2e420e',
+        },
+    },
+});
 
-export default function UploadButton() {
-  const classes = useStyles();
-
-  return (
-    <div>
-        <label htmlFor="outlined-button-file">
-        <Link to="/results" style={{ textDecoration: 'none' }}>
-          <Button variant="contained"  className={classes.button}>
-            Analyze Audio
-          </Button>
-          </Link>
-        </label>
-    </div>
-  );
+class AnalyzeButton extends React.Component {
+    render() {
+        const { classes } = this.props;
+        console.log("HELLO")
+            return (
+                <div>
+                    <label htmlFor="outlined-button-file">
+                        {this.props.bool ? (
+                            <Link to={'/results'} style={{ textDecoration: 'none' }}>
+                                <Button disabled={false} variant="contained" className={classes.button}>
+                                Analyze Audio
+                                </Button>
+                            </Link>
+                            ) : (
+                                <Button disabled={true} variant="contained" className={classes.button}>
+                                Analyze Audio
+                                </Button>
+                        )}
+                    </label>
+                </div>
+            )
+    }
 }
+export default withStyles(useStyles)(AnalyzeButton);
