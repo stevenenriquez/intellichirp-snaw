c802d45515ba059293b913bbb5672e701573918f
Mike Ewers <creativedevvers@gmail.com>
2020-02-14 05:06:54 -0700

    App.js changed. Added a submit button handler which will allow the analyze button to be toggled on/off depending on if files have been uploaded

--- snaw-frontend/src/App.js
+++ snaw-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import logo from './img/logo_small.png';
 import './App.css';
 import AnalyzeButton from './components/AnalyzeButton';
@@ -10,18 +9,33 @@ import Button from "@material-ui/core/Button";
 
 class App extends React.Component {
 
+
   constructor(props) {
     super(props);
-    this.state = {selectedFile: []}
+    this.state = {selectedFile: [], filesInserted: false, fileCount: 0};
+    this.submitHandler.bind(this)
   }
 
   fileSelectedHandler = event => {
-    //event.preventDefault();
-    this.state.selectedFile = Array.from(event.target.files)
+    event.preventDefault();
+    this.state.selectedFile = Array.from(event.target.files);
+    this.state.fileCount += this.state.selectedFile.length;
   };
 
-  render() {
+ submitHandler = event =>{
+     if(this.state.fileCount == 0) {
+         event.preventDefault();
+         this.render();
+     }
+     else{
+        this.state.filesInserted = true;
+        this.state.fileCount = 0;
+        this.forceUpdate();
 
+     }
+ }
+
+  render() {
     return (
       <div className="App">
         <ApplicationBar/>
@@ -33,20 +47,26 @@ class App extends React.Component {
           <br/>
           <form action="/uploader" method="POST"
                 encType="multipart/form-data">
-            {/*<InputLabel htmlFor="my-input">Upload File</InputLabel>*/}
-            <label htmlFor='my-input'><Button variant="outlined" color='#3f5a14' component='span'>Upload Audio
-              File</Button></label>
+            <label htmlFor='my-input'>
+                <Button variant="outlined" color='#3f5a14' component='span'>
+                    Upload Audio File
+                </Button>
+            </label>
             <input id="my-input" aria-describedby="my-helper-text" type='file' multiple={true} onChange={this.fileSelectedHandler} name='file' style={{display: 'none'}}/>
             <FormHelperText id="my-helper-text">
                 Drag and drop WAV files here, or click the Upload button to select file/s.
             </FormHelperText>
+              <label htmlFor='my-submit'>
+                  <input id='my-submit' type='submit' style={{display: 'none'}}/>
 
-            <label htmlFor='my-submit'><Button variant="contained" backgroundColor='#3f5a14'
-                                               component='span'>Submit</Button></label>
-            <Input id='my-submit' type='submit' style={{display: 'none'}}/>
+                  <Button variant="contained" backgroundColor='#3f5a14'onClick={this.submitHandler} component='span'>
+                      Submit
+                  </Button>
+              </label>
           </form>
-          <br/>
-            <AnalyzeButton/>
+            <br/>
+          {<AnalyzeButton bool={this.state.filesInserted}/>}
+          {this.state.filesInserted = false}
         </header>
       </div>
     );
